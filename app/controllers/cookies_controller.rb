0b1d8e058a7e0cb5b25180b318848d0ed0e9de5a
Augusto Goldoni <augusto@jaya.tech>
2018-10-01 19:12:27 -0300

    Enable creation of cookie

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -1,5 +1,20 @@
 class CookiesController < ApplicationController
   def index
     @cookies = request.cookies
+    @cookie = Cookie.new
+  end
+
+  def create
+    key = cookies_params[:key]
+    value = cookies_params[:value]
+    cookies[key] = value
+
+    redirect_to cookies_path
+  end
+
+  private
+
+  def cookies_params
+    params.require(:cookie).permit(:key, :value)
   end
 end
---
+++ app/models/cookie.rb
@@ -0,0 +1,4 @@
+class Cookie
+  include ActiveModel::Model
+  attr_accessor :key, :value
+end
